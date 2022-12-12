ef304e9c7c3c257eedea81ff1c7163e6a0dab1b6
ilij4 <ilijammilosavljevic@gmail.com>
2022-12-12 01:30:08 +0100

    mail

--- Mail/Startup.cs
+++ Mail/Startup.cs
@@ -27,12 +27,6 @@ namespace Mail
 
             services.AddDbContext<MailContext>(options =>
             options.UseSqlServer(Configuration.GetConnectionString("IdentityConnection")));
-
-            // In production, the Angular files will be served from this directory
-            services.AddSpaStaticFiles(configuration =>
-            {
-                configuration.RootPath = "ClientApp/dist";
-            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -57,19 +51,6 @@ namespace Mail
                 routes.MapRoute(
                     name: "default",
                     template: "{controller}/{action=Index}/{id?}");
-            });
-
-            app.UseSpa(spa =>
-            {
-                // To learn more about options for serving an Angular SPA from ASP.NET Core,
-                // see https://go.microsoft.com/fwlink/?linkid=864501
-
-                spa.Options.SourcePath = "ClientApp";
-
-                if (env.IsDevelopment())
-                {
-                    spa.UseAngularCliServer(npmScript: "start");
-                }
             });
         }
     }
